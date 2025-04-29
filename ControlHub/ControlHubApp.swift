cc2c29bcdec0574fd5bb791634de7c06b5e90652
Maulik Mann <maulikmann@gmail.com>
2025-04-29 16:39:23 -0400

    Initial Commit

---
+++ ControlHub/ControlHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlHubApp.swift
+//  ControlHub
+//
+//  Created by Maulik Mann on 2025-04-29.
+//
+
+import SwiftUI
+
+@main
+struct ControlHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ControlHubTests/ControlHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  ControlHubTests.swift
+//  ControlHubTests
+//
+//  Created by Maulik Mann on 2025-04-29.
+//
+
+import Testing
+@testable import ControlHub
+
+struct ControlHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
