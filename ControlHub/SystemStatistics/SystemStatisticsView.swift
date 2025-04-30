60ff62ce913e4610af5583aa1a7cd2feab102b5f
Maulik Mann <maulikmann@gmail.com>
2025-04-30 19:42:04 -0400

    Added files and folder saving in clipboard manager

---
+++ ControlHub/SystemStatistics/SystemStatisticsView.swift
@@ -0,0 +1,8 @@
+//
+//  SystemStatisticsView.swift
+//  ControlHub
+//
+//  Created by Maulik Mann on 2025-04-30.
+//
+
+import Foundation
---
+++ SystemStatistics.swift
@@ -0,0 +1,8 @@
+//
+//  SystemStatistics.swift
+//  ControlHub
+//
+//  Created by Maulik Mann on 2025-04-30.
+//
+
+import Foundation
